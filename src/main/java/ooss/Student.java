59eba45ebe0948bfc41562631e39540384141ba7
Antony Choi <antony.choi@oocl.com>
2022-11-30 17:57:03 +0800

    feat: implement class and update student

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,11 +1,19 @@
 package ooss;
 
 public class Student extends Person {
+    private Klass klass;
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
     @Override
     public String introduce(){
-        return super.introduce() + " I am a student.";
+        return super.introduce() + " I am a student. I am in class " + this.klass.getNumber() + ".";
+    }
+    public void join(Klass klass){
+        this.klass = klass;
+    }
+
+    public Boolean isIn(Klass klass){
+        return klass.equals(this.klass);
     }
 }
